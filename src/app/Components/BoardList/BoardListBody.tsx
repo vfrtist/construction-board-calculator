d7f9a82d4490cc7b638509af8912792f31d6bf37
vfrtist <137813892+vfrtist@users.noreply.github.com>
2026-07-15 16:55:22 -0400

    Separated optimizer to library folder

--- src/app/Components/BoardList/BoardListBody.tsx
+++ src/app/Components/BoardList/BoardListBody.tsx
@@ -1,41 +1,16 @@
 import { useContext, useMemo } from "react";
 import { CardContext } from "../Generic/Card";
-import { BoardList, CutBoard } from "../../Utility/calculator";
+import { getCutBoards, getCompactCutBoards } from "../../lib/optimizer";
 import Board from "./Board";
 import "../../Stylings/BoardList.css";
 
-function* arrayReducer(
-  boards: CutBoard[]
-): Generator<[qty: number, value: CutBoard]> {
-  if (boards.length === 0) {
-    return;
-  }
-  if (boards.length === 1) {
-    yield [1, boards[0]];
-    return;
-  }
-  let left = boards[0];
-  let count = 1;
-  for (let index = 1; index < boards.length; index++) {
-    const right = boards[index];
-    if (left.equals(right)) {
-      count++;
-    } else {
-      yield [count, left];
-      left = right;
-      count = 1;
-    }
-  }
-  yield [count, left];
-}
-
 export default function BoardListBody() {
   const { boardLength, cutInputs } = useContext(CardContext);
-  const boardList = useMemo(() => BoardList.fromBoardData(boardLength, cutInputs), [boardLength, cutInputs]);
+  const boardList = useMemo(() => getCutBoards(boardLength, cutInputs), [boardLength, cutInputs]);
 
   // Possible to implement a display style option for compact vs expanded view which would toggle this on/off.
   // For now compact is the only view and the other option would be to map over the boardList instead of compact List
-  const compactList = useMemo(() => arrayReducer(boardList), [boardList]);
+  const compactList = useMemo(() => getCompactCutBoards(boardList), [boardList]);
 
   return (
     <div className="BoardListBody">
---
+++ src/app/lib/optimizer.ts
@@ -0,0 +1,37 @@
+import { CutInput } from "../Data/Structures";
+import { BoardList, CutBoard } from "./BoardList";
+
+export function getCutBoards(length: number, cutInputs: CutInput[]): CutBoard[] {
+    const list = new BoardList(length);
+    list.setCuts(BoardList.expandCuts(cutInputs));
+    return list.boardList;
+}
+
+function* arrayReducer(
+    boards: CutBoard[]
+): Generator<[qty: number, value: CutBoard]> {
+    if (boards.length === 0) {
+        return;
+    }
+    if (boards.length === 1) {
+        yield [1, boards[0]];
+        return;
+    }
+    let left = boards[0];
+    let count = 1;
+    for (let index = 1; index < boards.length; index++) {
+        const right = boards[index];
+        if (left.equals(right)) {
+            count++;
+        } else {
+            yield [count, left];
+            left = right;
+            count = 1;
+        }
+    }
+    yield [count, left];
+}
+
+export function getCompactCutBoards(cutBoards: CutBoard[]): [qty: number, value: CutBoard][] {
+    return [...arrayReducer(cutBoards)];
+}
